63c68527665c03dfc1002f24b53428c2ebaef70f
Hyuk Min <judicious210@gmail.com>
2020-05-06 16:16:35 +0900

    Add JPA Auditing

---
+++ src/main/java/com/jud210/book/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.jud210.book.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+// JPA Entity 클래스들이 BaseTimeEntity를 상속할 경우,
+// 필드들(createdDate, modifiedDate)도 칼럼으로 인식하도록 함.
+@EntityListeners(AuditingEntityListener.class)
+// BaseTimeEntity 클래스에 Auditing 기능을 포함시킴.
+public abstract class BaseTimeEntity {
+
+    @CreatedDate // Entity 생성시, 생성 시간 자동 저장
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate // Entity 수정시, 수정 시간 자동 저장
+    private LocalDateTime modifiedDate;
+}
--- src/main/java/com/jud210/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jud210/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jud210.book.springboot.domain.posts;
 
+import com.jud210.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter // 클래스 내 모든 필드의 Getter 메소드 자동 생성
 @NoArgsConstructor // 기본 생성자 자동 추가 == public Posts() {}
 @Entity // 테이블과 링크될 클래스 (SalesManager -> sales_manager table)
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // PK 필드
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK 생성 규칙 for auto_increment
